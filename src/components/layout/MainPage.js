41745434382b1d8e23da3a802e91d307687df671
sahafonov <sahafonov@atlassian.com>
2023-03-21 22:10:42 +0200

    [APP] added ErrorOverlay

--- src/components/layout/MainPage.js
+++ src/components/layout/MainPage.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import MainNavigation from './MainNavigation';
 import classes from './MainPage.module.css';
 import ErrorOverlay from '../ui/ErrorOverlay';
-import { useDispatch, useSelector } from 'react-redux';
-import { authActions } from '../../store/auth';
 
 const MainPage = (props) => {
   const { errors } = useSelector((state) => state.allMovies);
-  const dispatch = useDispatch();
 
-  dispatch(authActions.login());
   return (
     <div className={classes.container}>
       <MainNavigation/>
--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { retrieveTokenFromCookie } from './cookies';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../store/auth';
 
 export const ProtectedRoute = ({ children, path }) => {
-  const isAuthenticated = retrieveTokenFromCookie();
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const token = retrieveTokenFromCookie();
+  const dispatch = useDispatch();
+
+  if (!isAuthenticated && token) {
+    dispatch(authActions.login());
+  }
+
   return (
     <Route path={path} render={() => isAuthenticated ? children : <Redirect to='/login' />} />
   );
